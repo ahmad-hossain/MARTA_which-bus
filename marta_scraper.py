19359a094bf5cb6f3b90eabe815857e2b796de99
Ahmad Hossain <ahmad.awaab@gmail.com>
2021-10-21 18:18:22 -0400

    edited some comments, completed get_stations(), and deleted unneeded method parse_table()

--- marta_scraper.py
+++ marta_scraper.py
@@ -9,7 +9,8 @@ TRAIN_LINES_URL = 'https://www.itsmarta.com/railline-schedules.aspx'
 TRAIN_STATIONS_URL = 'https://itsmarta.com/train-stations-and-schedules.aspx'
 
 def main():
-    
+    #prompt the user to enter the station they are leaving from
+    #as well as the destination bus stop
     make_choices()
 
 #todo in future, let train and bus be chosen at same time
@@ -26,38 +27,57 @@ def make_choices():
         bus_stop_choice = input('Enter the desired BUS STOP: ')
 
         #todo check if station and bus are valid
-        if valid_choices():
+        if valid_choices(station_choice, bus_stop_choice):
             valid_input = True
             
         #if user enters invalid input
         else:
             print("Invalid input. Please try again.")
 
+#checks if the inputs are a valid train station and bus stop
 def valid_choices(station: str, bus_stop: str) -> bool:
-    #todo check if station exists first by either using station list OR requesting webiste
+    
+    #get dictionary of stations and their page urls
+    stations_dict = get_stations()
+    
+    #if user entered station not in stations_dict keys
+    if station.lower() not in stations_dict:
+        return False
+    #user entered station does exist, so check if user can reach desired bus stop from the station
+    else:
+    #todo check saved dataframes for the desired stop. dfs will be saved as csv in a folder that has the rail station and bus stop name
+
+    #todo if saved dataframes not found, parse and save dataframes that have the desired stop
+
+    #todo check the schedule for every bus at the station and see if bus_stop is a destination. If it is, save it
+    #check if any key in get_buses() starts 
+        return
+    
+def get_stations() -> dict[str, str]:
     soup = get_soup(TRAIN_STATIONS_URL)
 
     #get the div that holds all the station names and url elements 
-    container = soup.find('div', class_='stations__items isotope')
+    stations_container = soup.find('div', class_='stations__items isotope')
 
-    #create dict for stations where station name is key and url is the value
+    #create dict for stations where station name is key and url is the value.
     stations_dict = {}
 
-    #loop through all the train stations in the container div
-    for station_element in container.findAll('a', href=True):
-        #populate dictionary with station information
-        stations_dict[station_element.text] = station_element['href']
+    #loop through all the train stations in the container div. All stations are in an <a> element with the text as the station name
+    #and the href as the relative path to the station page
+    for station_element in stations_container.findAll('a', href=True):
+        
+        #populate dictionary with station information. Key is lowercase. Example: {'airport': 'Airport.aspx'}
+        stations_dict[station_element.text.lower()] = station_element['href']
+    
+    return stations_dict
 
-
-    #todo if station DOES exist, check saved dataframes for the desired stop
-    #todo if saved dataframes not found, parse and save dataframes that have the desired stop
-    #? only save dfs for the desired stops??
-
+def get_buses() -> dict[str, str]:
+    return
 
 def get_soup(url: str) -> BeautifulSoup:
     html = requests.get(url)
 
-    #check for a response error
+    #raise exception if there's a response error
     try:
         html.raise_for_status()
     except:
@@ -65,21 +85,16 @@ def get_soup(url: str) -> BeautifulSoup:
     
     return BeautifulSoup(html.text, 'html.parser')
 
-
 #parse table for a MARTA bus page
 def parse_bus_table(df: pd.core.DataFrame) -> pd.core.frame.DataFrame:
     #When parsing bus table, the 1st col is all NaN values.
     #Check if first col has NaN values
     if df.iloc[:,0].isnull().values.any():
-        df.drop(df.columns[0], axis=1) #inplace=true
+        df = df.drop(df.columns[0], axis=1)
 
+    #alternative method:
     #drop all columns that have NaN values
     #df.dropna(axis=1, inplace=True)
-    return
-
-
-#parse a table for any MARTA page with a table    
-def parse_table(url: str) -> pd.core.frame.DataFrame:
     return
 
 if __name__ == "__main__":
