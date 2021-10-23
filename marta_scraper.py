b821f6149320495df7de6efc8fff8e0aae4544b6
Ahmad Hossain <ahmad.awaab@gmail.com>
2021-10-23 17:20:23 -0400

    added functionality for checking for saved bus schedules and implemented bus_urls() to get urls for buse pages when schedules aren't saved

--- marta_scraper.py
+++ marta_scraper.py
@@ -1,12 +1,15 @@
 import pandas as pd
 import requests
 from bs4 import BeautifulSoup
+import os, glob
 
 ROOT_URL = 'https://itsmarta.com/'
 EXT = '.aspx'
 BUS_ROUTES_URL = 'https://www.itsmarta.com/bus-routes.aspx'
 TRAIN_LINES_URL = 'https://www.itsmarta.com/railline-schedules.aspx'
 TRAIN_STATIONS_URL = 'https://itsmarta.com/train-stations-and-schedules.aspx'
+
+DF_FOLDER_PATH = '.\dataframes'
 
 def main():
     #prompt the user to enter the station they are leaving from
@@ -37,7 +40,7 @@ def make_choices():
 #checks if the inputs are a valid train station and bus stop
 def valid_choices(station: str, bus_stop: str) -> bool:
     
-    #get dictionary of stations and their page urls
+    #get dictionary of stations and their page urls. Example: {'airport': 'Airport.aspx'}
     stations_dict = get_stations()
     
     #if user entered station not in stations_dict keys
@@ -45,14 +48,31 @@ def valid_choices(station: str, bus_stop: str) -> bool:
         return False
     #user entered station does exist, so check if user can reach desired bus stop from the station
     else:
-    #todo check saved dataframes for the desired stop. dfs will be saved as csv in a folder that has the rail station and bus stop name
+        #todo check saved dataframes for the desired stop. dfs will be saved as csv in a folder that has the rail station and bus stop name
+        
+        #returns a list of all file paths in given path that contain the string f'{station}_{bus_stop}'
+        df_path_matches = glob.glob( os.path.join(DF_FOLDER_PATH, f'{station}_{bus_stop}' + '*.csv') )
+        
+        if len( df_path_matches ) > 0: # there is a df that matches
+            
+            #accumulates the data needed from all bus_schedules so it can be converted to a DataFrame
+            final_data = []
+            
+            #loop through every DataFrame in the list so we can get relevant data
+            for bus_schedule_path in df_path_matches:
+                #read the bus schedule as a DataFrame
+                df = pd.read_csv(bus_schedule_path)
 
-    #todo if saved dataframes not found, parse and save dataframes that have the desired stop
-
-    #todo check the schedule for every bus at the station and see if bus_stop is a destination. If it is, save it
-    #check if any key in get_buses() starts 
-        return
+                #filter df by time and bus_stop
+        else:
+            #todo if saved dataframes not found, parse and save dataframes that have the desired stop
+            #todo check the schedule for every bus at the station and see if bus_stop is a destination. If it is, save it
+         
+            return        
     
+    return
+    
+#get all marta train station names and the relative path for their websites
 def get_stations() -> dict[str, str]:
     soup = get_soup(TRAIN_STATIONS_URL)
 
@@ -71,8 +91,17 @@ def get_stations() -> dict[str, str]:
     
     return stations_dict
 
-def get_buses() -> dict[str, str]:
-    return
+#returns a list of MARTA bus page relative urls given a string of the url for a train station
+def get_bus_urls(station_url: str) -> list[str]:
+    soup = get_soup(station_url)
+    
+    #ul that contains li elements with bus name
+    bus_container = soup.find('ul', class_='station-info__side-block-list')
+
+    #get the href from every 'a' element in the bus_container 
+    bus_urls = [x['href'] for x in bus_container.findAll('a', href=True)]
+
+    return bus_urls
 
 def get_soup(url: str) -> BeautifulSoup:
     html = requests.get(url)
@@ -85,7 +114,7 @@ def get_soup(url: str) -> BeautifulSoup:
     
     return BeautifulSoup(html.text, 'html.parser')
 
-#parse table for a MARTA bus page
+#todo parse table for a MARTA bus page
 def parse_bus_table(df: pd.core.DataFrame) -> pd.core.frame.DataFrame:
     #When parsing bus table, the 1st col is all NaN values.
     #Check if first col has NaN values
